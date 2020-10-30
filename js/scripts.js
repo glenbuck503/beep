d1262156fdcbb613e71bfd3b91d823a010cd9920
glenbuck503 <glenbuck503@gmail.com>
2020-10-30 12:18:47 -0700

    add replacements for numbers 1 2 and 3

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
     let userNumber = $("input#userInput").val()
     let numArray = [0]
     const words = [1]
+    const words2 = [2]
+    const words3 = [3]
 
     
 
@@ -19,6 +21,10 @@ $(document).ready(function() {
     for(let i=0; i < numArray.length; i += 1)
      if (words.includes(numArray[i])) {
         numArray[i] = "beep";
+     } else if (words2.includes(numArray[i])) {
+       numArray[i] = "bop";
+     } else if (words3.includes(numArray[i])) {
+       numArray[i] = "won't you be my neighbor"
      }
 
 
