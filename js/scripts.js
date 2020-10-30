c3b6b9f7cf0b374175912877dd2eff2017dd5921
glenbuck503 <glenbuck503@gmail.com>
2020-10-30 12:13:57 -0700

    add word replacement

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,31 @@ $(document).ready(function() {
 
     let userNumber = $("input#userInput").val()
     let numArray = [0]
+    const words = [1]
+
+    
+
     function elementList(userNumber) {
       while (userNumber > numArray.length - 1) {
         numArray.push(numArray.length);
     
       };
     };
-    elementList(userNumber)
-    console.log(numArray);
+    elementList(userNumber);
 
+    for(let i=0; i < numArray.length; i += 1)
+     if (words.includes(numArray[i])) {
+        numArray[i] = "beep";
+     }
+
+
+    
+    
+    
+    
+    
+    
+    
+    console.log(numArray);
   });
 });
--- tests.md
+++ tests.md
@@ -1,5 +1,11 @@
 test: takes users number input string and turns string into number
 expect: userInput = parseint(userInput)
 
-test: put user number into an array with array starting with 0
-expect: new array = [0, userinput]
+test: put user number into an empty array
+expect: let array = []
+
+test: create an array of a range of numbers starting from 0 to users number
+exepct: number array to equal [0 to users number]
+
+test: make const array of replacement words or sentences
+expect: const words = "words,sentences"
