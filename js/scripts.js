fcb8df068c7d79bdeb3718d96cc6cbd90311623a
glenbuck503 <glenbuck503@gmail.com>
2020-11-04 22:06:38 -0800

    separate UI and Business Logic and fix jqeury to let users keep trying with different numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
-$(document).ready(function() {
-  $("form#beep").submit(function() {
-    event.preventDefault();
+    // Business Logic
 
-  let userInput = $("input#number").val();
-
-  function replace(userInput) {
+    function replace(userInput) {
     const numbers = [];
     for (let i = 0; i <= userInput; i++) {
       let element = i.toString();
-
+      
       if (element.includes(3)) {
         numbers.push("Won't you be my neighbor"+ ", ")
       } else if (element.includes(2)) {
@@ -22,10 +18,19 @@ $(document).ready(function() {
       }
       return numbers;
       }
+
+      // UI Logic
+
+      $(document).ready(function() {
+        $("form#beep").submit(function() {
+          event.preventDefault();
       
+      let userInput = $("input#number").val();
       let final = replace(userInput);
+  
+      $("#output").hide();
       $("#output").show();
-      $("#output").append(final);
+      $("#output").text(final);
     });
   });
 
@@ -51,42 +56,6 @@ $(document).ready(function() {
 
 
 
-// $(document).ready(function() {
-//   $("#rogers").submit(function(event) {
-//     event.preventDefault();
-
-//     let userNumber = parseInt($("input#userInput").val());
-//     let numArray = [0]
-//     const words = [1]
-//     const words2 = [2]
-//     const words3 = [3]
-//     const wild = [3]
-    
-
-   
-    
-
-    
-
-//     function elementList(userNumber) {
-//       while (userNumber > numArray.length - 1) {
-//         numArray.push(numArray.length);
-    
-//       };
-//     };
-//     elementList(userNumber);
-  
-
-//     for(let i=0; i < numArray.length; i += 1)
-//      if (words.includes(numArray[i])) {
-//         numArray[i] = "beep";
-//      } else if (words2.includes(numArray[i])) {
-//        numArray[i] = "bop";
-//      } else if (words3.includes(numArray[i])) {
-//        numArray[i] = "won't you be my neighbor"
-//      } 
-     
-
    
 
 
@@ -96,6 +65,3 @@ $(document).ready(function() {
     
     
     
-//     console.log(numArray);
-//   });
-// });
